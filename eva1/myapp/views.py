466944e66d9393634334223f6ae2f0d9b8ba1523
camilobvstn <169169857+camilobvstn@users.noreply.github.com>
2024-09-11 10:48:36 -0300

    ahora

--- eva1/myapp/views.py
+++ eva1/myapp/views.py
@@ -6,13 +6,13 @@ def main(request):
     return render (request,'myapp/index.html')
 
 def manzana(request):
-    data={"nombre":"Tarta de Manzana", "desc":"Una deliciosa tarta de manzana"}
+    data={"nombre":"Tarta de Manzana", "desc":"Una deliciosa tarta de manzana","img_1":"mazana.jpeg"}
     return render(request,'myapp/recetas.html',data)
 
 def paella(request):
-    data={"nombre":"Paella", "desc":"Un delicioso plato tipico español"}
+    data={"nombre":"Paella", "desc":"Un delicioso plato tipico español","img_1":"paella.jpeg"}
     return render(request,'myapp/recetas.html',data)
 
 def pizza(request):
-    data={"nombre":"Pizza", "desc":"Un delicioso plato tipico italiano"}
+    data={"nombre":"Pizza", "desc":"Un delicioso plato tipico italiano","img_1":"pizza.jpeg"}
     return render(request,'myapp/recetas.html',data)
