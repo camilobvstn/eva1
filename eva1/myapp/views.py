f8f49fc9f19c564f29494b447b2e44dbe90d0c26
camilobvstn <camilommorales2004@gmail.com>
2024-09-12 21:33:42 -0300

    con fotos?

--- eva1/myapp/views.py
+++ eva1/myapp/views.py
@@ -6,13 +6,13 @@ def main(request):
     return render (request,'myapp/index.html')
 
 def manzana(request):
-    data={"nombre":"Tarta de Manzana", "desc":"Una deliciosa tarta de manzana","img_1":"mazana.jpeg"}
+    data={"nombre":"Tarta de Manzana", "desc":"Una deliciosa tarta de manzana","img_1":"img/manaza.jpeg"}
     return render(request,'myapp/recetas.html',data)
 
 def paella(request):
-    data={"nombre":"Paella", "desc":"Un delicioso plato tipico español","img_1":"paella.jpeg"}
+    data={"nombre":"Paella", "desc":"Un delicioso plato tipico español","img_1":"img/paella.jpeg"}
     return render(request,'myapp/recetas.html',data)
 
 def pizza(request):
-    data={"nombre":"Pizza", "desc":"Un delicioso plato tipico italiano","img_1":"pizza.jpeg"}
+    data={"nombre":"Pizza", "desc":"Un delicioso plato tipico italiano","img_1":"img/pizza.jpeg"}
     return render(request,'myapp/recetas.html',data)
